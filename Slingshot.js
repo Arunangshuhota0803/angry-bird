48184fffc3d6f95991dd4c33174cdcaf99f52dc3
Arunangshuhota0803 <78540271+Arunangshuhota0803@users.noreply.github.com>
2021-06-10 04:28:20 -0700

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,38 @@
+class Slingshot 
+{
+constructor(bodyA,bodyB)
+{
+var option = {
+bodyA : bodyA,
+bodyB:BodyB ,
+lenght = 10,
+stiffness=0.04
+}
+this.Slingshot = matter.constarint.create(option)
+world.add(world.this.Slingshot);
+}
+
+display()
+{
+var pointA = this.Slingshot.bodyA.position;
+var pointB = this.Slingshot.bodyB.position ;
+  
+
+// line (pointAx,pointAy,pointBx,pointBy)
+strokeWeight(4);
+line (pointA.x,pointA.y,pointB.x,pointB.y)
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+}
